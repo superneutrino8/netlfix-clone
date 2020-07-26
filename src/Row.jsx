bd0cf78fe412b51bb77da7792f7648978da40833
Vivek Bariya <vivekbaraiya7@gmail.com>
2020-07-26 23:18:37 +0530

    changed trending to country wise

--- src/Row.jsx
+++ src/Row.jsx
@@ -1,12 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from './axios';
 
-function Row({ title }) {
+function Row({ title, fetchUrl }) {
     const [movie, setMovie] = useState([]);
 
     useEffect(() => {
-        
-    }, [])
+        // if [], run once when row loads, and don't run again
+        async function fetchData() {
+            const request = await axios.get(fetchUrl);
+            console.log(request);
+            return request;
+        }
+        fetchData();
+    }, [fetchUrl])
 
     return (
         <div>
