4f5767cb8553b319b718b637b6aad8e022ce677b
Vivek Bariya <vivekbaraiya7@gmail.com>
2020-07-27 23:24:21 +0530

    fetched banner data randomly

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import requests from "./request";
+import axios from "./axios";
 
 function Banner() {
-    return <div></div>;
+    const [movie, setMovie] = useState([]);
+
+    useEffect(() => {
+        async function getMovie() {
+            const request = await axios.get(requests.fetchNetflixOriginal);
+            setMovie(
+                request.data.results[
+                    Math.floor(Math.random() * request.data.results.length - 1)
+                ]
+            );
+            return request;
+        }
+        getMovie();
+    }, []);
+
+    return (
+        <header
+            className="banner"
+            style={{
+                backgroundSize: "cover",
+                backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+                backgroundPosition: "center center",
+            }}
+        >
+            <div className="banner__contents"></div>
+        </header>
+    );
 }
 
 export default Banner;
