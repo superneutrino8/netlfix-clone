3effc3583055898673d2cdf16269cfb17f66c7d4
Vivek Bariya <vivekbaraiya7@gmail.com>
2020-07-29 17:01:54 +0530

    added youtube popup

--- src/Row.jsx
+++ src/Row.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 import axios from "./axios";
 import "./Row.css";
@@ -7,6 +9,7 @@ const BASE_URL = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl, isLargeRow }) {
     const [movies, setMovies] = useState([]);
+    const [trailerUrl, setTrailerUrl] = useState("");
 
     useEffect(() => {
         // if [], run once when row loads, and don't run again
@@ -17,6 +20,35 @@ function Row({ title, fetchUrl, isLargeRow }) {
         fetchData();
     }, [fetchUrl]);
 
+    if (movies)
+        movies.sort((a, b) => (a.vote_average < b.vote_average ? 1 : -1));
+
+    const opts = {
+        height: "390",
+        width: "100%",
+        playerVars: {
+            autoplay: 1,
+            origin: "http://localhost:3000",
+        },
+    };
+
+    const handleClick = (movie) => {
+        console.log("Click");
+        if (trailerUrl) {
+            setTrailerUrl("");
+        } else {
+            movieTrailer(movie?.name || "")
+                .then((url) => {
+                    console.log(url);
+                    const urlParamas = new URLSearchParams(new URL(url).search);
+                    setTrailerUrl(urlParamas.get("v"));
+                })
+                .catch((err) => console.log(err));
+        }
+    };
+
+    // console.log(movies);
+
     return (
         <div className="row">
             <h2>{title}</h2>
@@ -26,6 +58,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
                         key={movie.id}
                         className={`row__poster ${isLargeRow &&
                             "row__posterLarge"}`}
+                        onClick={() => handleClick(movie)}
                         src={`${BASE_URL}${
                             isLargeRow
                                 ? movie.poster_path
@@ -38,6 +71,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
                 ))}
             </div>
             {/* container -> posters */}
+            {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
         </div>
     );
 }
--- src/request.js
+++ src/request.js
@@ -1,7 +1,7 @@
 const API_KEY = "e60fdb10dc40ce34fbe239ea4e7e9641";
 
 export default {
-    fetchTrending: `/trending/all/day?api_key=${API_KEY}&certification_country=IN`,
+    fetchTrending: `/trending/all/day?api_key=${API_KEY}&certification_country=IN&region=IN`,
     fetchNetflixOriginal: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
     fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
     fetchActionMovie: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
